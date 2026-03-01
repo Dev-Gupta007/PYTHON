c917400d8bb45106173b1c4b8355d1195089cdfd
Dev-Gupta007 <max007legend@gmail.com>
2026-03-01 17:41:38 +0530

    Updated Filtering.py

--- Pandas/06. Filtering.py
+++ Pandas/06. Filtering.py
@@ -13,3 +13,17 @@ legendary_pokemon = df[df["Legendary"] == 1]
 print(tall_pokemon)
 print(heavy_pokemon)
 print(legendary_pokemon)
+
+#Water Pokemon
+
+Water_Pokemon = df[(df["Type1"] == "Water")|
+                   (df["Type2"] == "Water")]
+
+print(Water_Pokemon)
+
+# Fire Flying Pokemon
+
+Fire_Flying_Pokemon = df[(df["Type1"] == "Fire") &
+                         (df["Type2"] == "Flying")]
+
+print(Fire_Flying_Pokemon)
