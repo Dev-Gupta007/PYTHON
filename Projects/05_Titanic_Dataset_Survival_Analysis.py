720b27223780fdbf12fcd07e8362c579c186e26e
Dev-Gupta007 <max007legend@gmail.com>
2026-03-02 18:07:41 +0530

    Made Output Cleaner

--- Projects/05_Titanic_Dataset_Survival_Analysis.py
+++ Projects/05_Titanic_Dataset_Survival_Analysis.py
@@ -6,6 +6,9 @@
 # | 80% / 20%    | Strongly imbalanced              |
 # | 90% / 10%    | Severely imbalanced              |
 
+print("="*50)
+print("TITANIC SURVIVAL ANALYSIS")
+print("="*50)
 
 import pandas as pd
 
@@ -13,19 +16,17 @@ df = pd.read_csv(r"D:\DEV\Programming\PYTHON\Projects\~~Titanic.csv")
 
 # Loading And Inspecting Data
 
-print(df)
-
-print(df.shape)
-print(df.columns)
-print(df.head())            # Getting Index
-print(df.info())
-print(df.isnull().sum())    # Identifying Nan Values
+# print(df.shape)
+# print(df.columns)
+# print(df.head())            # Getting Index
+# print(df.info())
+# print(df.isnull().sum())    # Identifying Nan Values
 
 # Data Cleaning
 
 # Fill age with median
 
-df["Age"] = df["Age"].fillna(df["Age"].median() , inplace = True)
+df["Age"] = df["Age"].fillna(df["Age"].median())
 
 # Droping Cabin (too many missing values)
 df.drop("Cabin", axis=1, inplace=True)
@@ -43,11 +44,17 @@ Survived = df[df["Survived"] == True]
 
 Percentage = (Survived.count()/df["Survived"].count())*100
 
+print()
+
 print(f"{Survived["Survived"].count()} people survived out of {df["Survived"].count()}")
 
-print(f"{Percentage["Survived"]} % People Survived" )
+print(f"{(Percentage["Survived"]).round(2)} % People Survived" )
 
+print()
+
+print("-"*50)
 print(f"The Data is Slightly Imbalanced")
+print("-"*50)
 
 Number_of_Males = (df[df["Sex"] == "male"].count())["Sex"]
 Number_of_Females = df[df["Sex"] == "female"].count()["Sex"]
@@ -55,7 +62,12 @@ Number_of_Females = df[df["Sex"] == "female"].count()["Sex"]
 print(f"""No. of Males Survived = {Number_of_Males}
 No. of Females Survived = {Number_of_Females}""")
 
-print(f"Male/Female Ratio = {Number_of_Males/Number_of_Females}")
+print(f"Male/Female Ratio = {(Number_of_Males/Number_of_Females):.2f}")
+
+print()
+print("-"*50)
+print("Class Wise Survival Rate")
+print("-"*50)
 
 # First Class Survival Rate
 
@@ -63,7 +75,7 @@ First_Class_Survivived = Survived[Survived["Pclass"]  == 1].count()["Pclass"]
 Total_First_Class_Passengers = df[df["Pclass"] == 1].count()["Pclass"]
 First_Class_Survival_Rate = First_Class_Survivived*100 / Total_First_Class_Passengers
 
-print(f"First Class Survival Rate = {First_Class_Survival_Rate}")
+print(f"First Class Survival Rate = {First_Class_Survival_Rate:.2f}")
 
 # Second Class Survival Rate
 
@@ -71,7 +83,7 @@ Second_Class_Survivived = Survived[Survived["Pclass"]  == 2].count()["Pclass"]
 Total_Second_Class_Passengers = df[df["Pclass"] == 2].count()["Pclass"]
 Second_Class_Survival_Rate = Second_Class_Survivived*100 / Total_Second_Class_Passengers
 
-print(f"Second Class Survival Rate = {Second_Class_Survival_Rate}")
+print(f"Second Class Survival Rate = {Second_Class_Survival_Rate:.2f}")
 
 # Third Class Survival Rate
 
@@ -79,7 +91,12 @@ Third_Class_Survivived = Survived[Survived["Pclass"]  == 3].count()["Pclass"]
 Total_Third_Class_Passengers = df[df["Pclass"] == 3].count()["Pclass"]
 Third_Class_Survival_Rate = Third_Class_Survivived*100 / Total_Third_Class_Passengers
 
-print(f"Third Class Survival Rate = {Third_Class_Survival_Rate}")
+print(f"Third Class Survival Rate = {Third_Class_Survival_Rate:.2f}")
+
+print()
+print("-"*50)
+print("Adult and Child Survival Rate")
+print("-"*50)
 
 # Adult and Child Passengers and Survival Rate
 
@@ -87,14 +104,15 @@ Total_Passengers = df["PassengerId"].count()
 No_of_Adult_Passengers = df[df["Age"] >= 18].count()["Age"]
 No_of_Child_Passengers = df[df["Age"] < 18].count()["Age"]
 
-print(f"Percentage of Adult Passengers = {No_of_Adult_Passengers*100/Total_Passengers}")
-print(f"Percentage of Child Passengers = {No_of_Child_Passengers*100/Total_Passengers}")
+print(f"Percentage of Adult Passengers = {(No_of_Adult_Passengers*100/Total_Passengers):.2f}")
+print(f"Percentage of Child Passengers = {(No_of_Child_Passengers*100/Total_Passengers):.2f}")
 
 Survived_Adult_Passengers = Survived[Survived["Age"] >= 18].count()["Age"]
 Survived_Child_Passengers = Survived[Survived["Age"] < 18].count()["Age"]
 
 Adult_Survival_Rate = Survived_Adult_Passengers*100/No_of_Adult_Passengers
 Child_Survival_Rate = Survived_Child_Passengers*100/No_of_Child_Passengers
-
-print(Adult_Survival_Rate)
-print(Child_Survival_Rate)
+print()
+print(f"Adult Survival Rate : {Adult_Survival_Rate:.2f}")
+print(f"Child Survival Rate : {Child_Survival_Rate:.2f}")
+print()
